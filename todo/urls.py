74353808aefa6240f80dd2fd52b2bb779a2e1956
hyunjooooojung <hyunjoo3107@gmail.com>
2022-11-15 17:49:16 +0900

    완료 todo list 조회하기

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TodoAPIView,TodoDetailAPIView
+from .views import TodoAPIView,TodoDetailAPIView, DoneTodoAPIView, DoneDetailTodoAPIView
 
 urlpatterns = [
     path('todo/', TodoAPIView.as_view()),
     path('todo/<int:todo_id>/',TodoDetailAPIView.as_view()),
+    path('done/', DoneTodoAPIView.as_view()),
+    path('done/<int:todo_id>/', DoneDetailTodoAPIView.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -28,3 +28,37 @@ class TodoDetailAPIView(APIView):
         todo = get_object_or_404(Todo, id=todo_id)
         serializer = TodoDetailSerializer(todo)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # todo list 수정하기
+    def put(self, request, todo_id):
+        todo = get_object_or_404(Todo, id=todo_id)
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # todo list 삭제하기
+    def delete(self, request, todo_id):
+        todo = get_object_or_404(Todo, id=todo_id)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        
+# 전체 완료 todo 조회
+class DoneTodoAPIView(APIView):
+    def get(self, request):
+        dones = Todo.objects.filter(complete=True)
+        serializer = TodoSerializer(dones, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+  
+# 완료 todo 상세 목록
+class DoneDetailTodoAPIView(APIView):
+    def get(self, request, todo_id):
+        done = get_object_or_404(Todo, id=todo_id)
+        done.complete = True
+        done.save()
+        serializer = TodoDetailAPIView(done)
+        return Response(status=status.HTTP_200_OK)
