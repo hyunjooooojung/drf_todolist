656b94da2defdf10e38f7e2df4b6abe7d2a32633
hyunjooooojung <hyunjoo3107@gmail.com>
2022-11-15 16:44:49 +0900

    todo list 생성하기 API 작성

--- todo/serializers.py
+++ todo/serializers.py
@@ -6,8 +6,14 @@ class TodoSerializer(serializers.ModelSerializer):
         model = Todo
         fields = ('id', 'title', 'complete', 'important')
         
-
+# todo list 상세 조회
 class TodoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'created_at', 'complete', 'important')
+        
+# todo list 작성하기
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'description', 'important')
--- todo/views.py
+++ todo/views.py
@@ -4,13 +4,24 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from .models import Todo
-from .serializers import TodoSerializer, TodoDetailSerializer
+from .serializers import TodoSerializer, TodoDetailSerializer, TodoCreateSerializer
 
 class TodoAPIView(APIView):
     def get(self, request):
         todos = Todo.objects.filter(complete=False)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        # 입력한 데이터(request.data)를 serializer에 넣어준다.
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            # serializer에 들어온 데이터가 올바른 데이터이면 저장.
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     
 class TodoDetailAPIView(APIView):
     def get(self, request, todo_id):
